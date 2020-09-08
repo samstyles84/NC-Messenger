d90a0b59a40b86d25a7b14fdc9096e660f344f7d
samstyles84 <sam.styles@arup.com>
2020-09-08 10:08:12 +0100

    Added get apis endpoint

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -1,10 +1,11 @@
 const apiRouter = require("express").Router();
 const usersRouter = require("./users.router");
 
-//const sendAPIs = require("../controllers/apis.controllers");
+const sendAPIs = require("../controllers/apis.controllers");
 
 apiRouter.use("/users", usersRouter);
 
-//apiRouter.route("/").get(sendAPIs).all(handle405s);
+apiRouter.route("/").get(sendAPIs);
+//.all(handle405s);
 
 module.exports = apiRouter;
